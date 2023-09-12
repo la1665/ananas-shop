d859a6e9c09a53c9b0d8afd60c824351a43b69bb
Mehryar Sadati <mehryarsadati@gmail.com>
2023-09-12 23:08:51 +0330

    seller_url added

--- pineapple/urls.py
+++ pineapple/urls.py
@@ -10,7 +10,11 @@ urlpatterns = [
     path("subscription-create/", views.views_subscription.subscription_create_view, name="subscription-create"),
     path("subscription-list/", views.views_subscription.subscription_list_view, name="subscription-list"),
     # Seller:
-    #TODO
+   path("seller-create/", views.views_sellers.seller_create_view, name="seller-create"),
+   path("seller-detail/<int:certificate_code>/", views.views_sellers.seller_detail_view, name="seller-detail"),
+   path("seller-list/", views.views_sellers.seller_list_view, name="seller-list"),
+   path("seller-update/<int:certificate_code>/", views.views_sellers.seller_update_view, name="seller-update"),
+
     # Pineapple:
     #TODO
     # Order:
