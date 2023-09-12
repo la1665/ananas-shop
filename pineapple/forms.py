3701019f17893e21c40b4f111ad4b09002c7f852
Mehryar Sadati <mehryarsadati@gmail.com>
2023-09-13 00:18:46 +0330

    seller form added

--- pineapple/forms.py
+++ pineapple/forms.py
@@ -1,7 +1,21 @@
-
+from django import forms
+from django.core.exceptions import ValidationError
 
 class SellerForm:
-    pass
+    
+    name = forms.CharField(max_length=256)
+    address = forms.CharField(widget=forms.Textarea)
+    certificate_code = forms.CharField(max_length=10)
+
+    def clean_address(self):
+        address = str(self.cleaned_data["address"])
+        if len(address) < 10:
+            raise ValidationError("این فیلد باید بیشتر از ۱۰ کاراکتر باشد.")
+
+    def clean_certificate_code(self):
+        certificate_code = str(self.cleaned_data["certificate_code"])
+        if certificate_code != certificate_code.upper():
+            raise ValidationError("حروف گواهینامه باید حروف بزرگ باشد.")
 
 class PineappleForm:
     pass
