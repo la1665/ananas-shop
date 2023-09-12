b2d67f918eaf93f17acdb84566d56fb90452dbf3
Sajad <imsajtj@gmail.com>
2023-09-12 19:38:46 +0330

    added subscription Pineapple paths for pineapple urls.

--- pineapple/urls.py
+++ pineapple/urls.py
@@ -11,7 +11,11 @@ urlpatterns = [
     # Seller:
     #TODO
     # Pineapple:
-    #TODO
+    path("pineapple-list/", views.views_pineapple.pineapple_list_view, name="pineapple-list"),
+    path("pineapple-detail/<int:pk>/", views.views_pineapple.pineapple_detail_view, name="pineapple-detail"),
+    path("pineapple-create/", views.views_pineapple.pineapple_create_view, name="pineapple-create"),
+    path("pineapple-update/<int:pk>/", views.views_pineapple.pineapple_update_view, name="pineapple-update"),
+    path("seller-pineapple-list/<int:seller_id>/", views.views_pineapple.seller_pineapple_list_view, name="seller-pineapple-list"),
     # Order:
     #TODO
     # Comment:
